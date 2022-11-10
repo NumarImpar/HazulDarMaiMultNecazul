918b87c20f79762b70cdaee1c9f8d519181f547f
AlexD70 <alex.do.mihai@gmail.com>
2022-11-10 19:39:06 +0200

    pid tests

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motortesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motortesting.java
@@ -1,3 +1,8 @@
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.control.PIDCoefficients;
+import com.acmerobotics.roadrunner.control.PIDFController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -5,14 +10,22 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
 @TeleOp
+@Config
 public class Motortesting extends LinearOpMode {
 
     public Lifter lifter;
-    public int maxHeightTicks = 3100;
-    public int minHeightTicks = -10;
+    public double maxHeightTicks = 3100, minHeightTicks = -10;
     public double delta = 0.5;
     public double stand = 0.1;
+    public static double kP = 11, kI = 0, kD = 0;
+    public static double targetPosition = 2800;
+    public double currentPosition;
+    public static double maxPower = 0.6;
+    double power, correction;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -23,9 +36,36 @@ public class Motortesting extends LinearOpMode {
 
         while(opModeIsActive()){
 
+            //go to target position with given coefficients
+            PIDCoefficients coeffs = new PIDCoefficients(kP, kI, kD);
+            PIDFController controller = new PIDFController(coeffs);
+            controller.setTargetPosition(targetPosition);
+
+            if(gamepad2.a == true) {
+                while (opModeIsActive()) {
+
+                    currentPosition = lifter.lifterEncoder.getCurrentPosition();
+
+                    correction = controller.update(currentPosition) / targetPosition;
+                    power = Range.clip(correction, 0.1, maxPower);
+                    lifter.setLifterPower(power);
+
+                    telemetry.addData("ticksCurrent", currentPosition);
+                    telemetry.addData("correction", correction);
+                    telemetry.addData("power", power);
+                    telemetry.update();
+
+
+                    if (gamepad2.b == true) {
+                        break;
+                    }
+
+                }
+            }
+
             //telemetry for debugging
             telemetry.addData("ticks:", lifter.leftLifter.getCurrentPosition());
-            telemetry.addData("trigger", gamepad1.right_trigger);
+            telemetry.addData("trigger", gamepad2.right_trigger);
             telemetry.update();
 
             //lifter
@@ -39,12 +79,12 @@ public class Motortesting extends LinearOpMode {
             }
 
             //limits! max = 3100, min = -10
-            if(lifter.leftLifter.getCurrentPosition() > maxHeightTicks && gamepad2.right_trigger != 0) {
-                lifter.setLifterPower(0);
+            if(lifter.leftLifter.getCurrentPosition() >= maxHeightTicks) {
+                lifter.setLifterPower(0.1);
             }
 
-            if(lifter.leftLifter.getCurrentPosition() <= minHeightTicks && gamepad2.left_trigger != 0) {
-                lifter.setLifterPower(0);
+            if(lifter.leftLifter.getCurrentPosition() <= minHeightTicks) {
+                lifter.setLifterPower(0.1);
             }
         }
     }
